61307ff0b87156b241a3b3a21ce47243627ecb17
Li Cheng <lc122798@alibaba-inc.com>
2018-05-08 11:36:32 +0800

    fixed pv_config.py grammer error

--- tools/testbed/testscripts/pv_config.py
+++ tools/testbed/testscripts/pv_config.py
@@ -3,34 +3,36 @@ testsuits = {
         'tests':{
             'alink5pps': {'script':'alink_testrun.py', 'firmware_prefix':'alinkapp@', 'firmware_suffix': '-general.bin', 'args':'--testname=short5pps'},
             'mesh5pps': {'script':'alink_mesh_testrun.py', 'firmware_prefix':'alinkapp@', 'firmware_suffix': '-general.bin', 'args':'--testname=short5pps'}
-        },
+            },
         'wifissid': 'aos_test_01',
         'wifipass': 'Alios@Embedded'
-    },
+        },
     'mesh': {
         'tests': {
-        'line_topology': {'script':'line_topology_v4_test.py', 'firmware_prefix':'alinkapp@', 'firmware_suffix': '-general.bin'},
-        'tree_topology': {'script': 'tree_topology_v4_test.py', 'firmware_prefix':'alinkapp@', 'firmware_suffix': '-general.bin'},
-        'multicast': {'script': 'multicast_v4_test.py', 'firmware_prefix':'alinkapp@', 'firmware_suffix': '-general.bin'}
-        },
+            'line_topology': {'script':'line_topology_v4_test.py', 'firmware_prefix':'alinkapp@', 'firmware_suffix': '-general.bin'},
+            'tree_topology': {'script': 'tree_topology_v4_test.py', 'firmware_prefix':'alinkapp@', 'firmware_suffix': '-general.bin'},
+            'multicast': {'script': 'multicast_v4_test.py', 'firmware_prefix':'alinkapp@', 'firmware_suffix': '-general.bin'}
+            },
         'wifissid': 'alibaba-test-1227981',
         'wifipass': 'Alios@Things'
-    },
+        },
     'net': {
         'tests': {
-        'domain': {'script': 'network_v4_test.py', 'firmware_prefix':'networkapp@', 'firmware_suffix': 'l432kc-nucleo-general.bin'}
-        },
+            'domain': {'script': 'network_v4_test.py', 'firmware_prefix':'networkapp@', 'firmware_suffix': 'l432kc-nucleo-general.bin'}
+            },
         'wifissid': 'alibaba-test-1227982',
         'wifipass': 'Alios@Things'
-    },
+        },
     'auth': {
         'tests': {
-        'meshauth': {'script': 'auth_v4_test', 'firmware_prefix':'meshapp@', 'firmware_suffix': '-meshauth.bin'}
-        }
-    },
+            'meshauth': {'script': 'auth_v4_test.py', 'firmware_prefix':'meshapp@', 'firmware_suffix': '-meshauth.bin'}
+            }
+        },
     'ut': {
-        'ut': {'script': 'aos_ut.sh', 'firmware_prefix': 'yts@', 'firmware_suffix':'.elf'}
-    }
+        'tests': {
+            'ut': {'script': 'aos_ut.sh', 'firmware_prefix': 'yts@', 'firmware_suffix':'.elf'}
+            }
+        }
 }
 
 models = {
